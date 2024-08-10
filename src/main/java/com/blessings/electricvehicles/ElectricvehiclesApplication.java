77e8eaa4881470091a90874ee7652ba107409bbb
ash <ashi24.ashish@gmail.com>
2024-08-10 21:21:13 +0530

    first commit

---
+++ src/main/java/com/blessings/electricvehicles/ElectricvehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.blessings.electricvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricvehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricvehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blessings/electricvehicles/ElectricvehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blessings.electricvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricvehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
